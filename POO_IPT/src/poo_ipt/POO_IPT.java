a6727a7640b7928d3d9da39857f9255c0c05194a
António Manso <manso@ipt.pt>
2024-02-15 10:09:10 +0000

    Hello 1

---
+++ POO_IPT/src/poo_ipt/POO_IPT.java
@@ -0,0 +1,33 @@
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: 
+//::                                                                         ::
+//::     Antonio Manuel Rodrigues Manso                                      ::
+//::                                                                         ::
+//::     I N S T I T U T O    P O L I T E C N I C O   D E   T O M A R        ::
+//::     Escola Superior de Tecnologia de Tomar                              ::
+//::     e-mail: manso@ipt.pt                                                ::
+//::     url   : http://orion.ipt.pt/~manso                                  ::
+//::                                                                         ::
+//::     This software was build with the purpose of investigate and         ::
+//::     learning.                                                           ::
+//::                                                                         ::
+//::                                                               (c)2024   ::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//////////////////////////////////////////////////////////////////////////////
+
+package poo_ipt;
+
+/**
+ * Created on 15/02/2024, 10:08:44 
+ * @author IPT - computer
+ */
+public class POO_IPT {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
